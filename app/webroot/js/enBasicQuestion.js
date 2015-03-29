56b1a5279d366e812b3c7441b63fd2369b4defe2
yutaka <ibzset@gmail.com>
2015-03-29 17:51:11 +0900

    modify enBasicQuestion

--- app/webroot/js/enBasicQuestion.js
+++ app/webroot/js/enBasicQuestion.js
@@ -5,22 +5,15 @@
 jQuery(function ($) {
 
     $(".main").css("text-align", "center");
-
     $("#next").css("float", "right");
     $("#previous").css("float", "left");
     $("#judge").css("color", "red");
-
-
-
     $("h2").css("color", "blue");
     $("#QuestionEnBasicForm").css("margin", "0px auto");
-
-
 
 });
 
 var answer = '';
 function displayAnswer(answer){
-    //var answer = 'test';
     alert(answer);
 }
